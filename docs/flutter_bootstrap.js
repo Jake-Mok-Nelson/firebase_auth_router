a018c6c11a567e7c2968dc4f27450b0cbfa290d9
jake-mok-nelson <Jake-Mok-Nelson@users.noreply.github.com>
2025-01-16 17:59:43 +1100

    Update example

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2664233858"
+    serviceWorkerVersion: "3406253689"
   }
 });
